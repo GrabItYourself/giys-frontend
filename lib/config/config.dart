852b565215438290a8956cd25babfd03c4835c84
Saenyakorn Siangsanoh <saenyakorn.si@gmail.com>
2022-10-31 22:24:39 +0700

    able to use Google OAuth in iOS

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,7 +3,7 @@ class Config {
   static const String appVersion = '1.0.0';
   static const String serverURL = 'http://localhost:8080';
   static const String googleClientId =
-      '964375831805-7b7j39ojal5kfbhdm92r11fmk730bhr1.apps.googleusercontent.com';
+      '964375831805-ptcf2kf7uuee47g7dhoj2ghhbvq5t9c1.apps.googleusercontent.com';
   static const String callbackUrlScheme =
-      'com.googleusercontent.apps.964375831805-7b7j39ojal5kfbhdm92r11fmk730bhr1';
+      'com.googleusercontent.apps.964375831805-ptcf2kf7uuee47g7dhoj2ghhbvq5t9c1';
 }
