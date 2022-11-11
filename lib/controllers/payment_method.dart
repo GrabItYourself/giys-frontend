f8290982928c991054d6e25a520735fc64238baf
nonya <nonya579@gmail.com>
2022-11-11 20:39:14 +0700

    connect backend

--- lib/controllers/payment_method.dart
+++ lib/controllers/payment_method.dart
@@ -24,44 +24,30 @@ class PaymentMethodController extends GetxController {
   }
 
   Future<List<PaymentMethod>> getMyPaymentMethods() async {
-    // final response = await Requests.get(
-    //   '${Config.serverUrl}/api/v1/user/me/paymentMethods',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // );
-    // response.raiseForStatus();
+    final response = await Requests.get(
+      '${Config.getServerUrl()}/api/v1/user/me/paymentMethods',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    response.raiseForStatus();
 
-    // final paymentMethods = <PaymentMethod>[];
-    // for (var item in response.json()) {
-    //   paymentMethods.add(PaymentMethod.fromJson(item));
-    // }
-    // return paymentMethods;
-
-    await Future.delayed(const Duration(seconds: 1));
     final paymentMethods = <PaymentMethod>[];
-    paymentMethods
-        .add(PaymentMethod(id: 1, lastFourDigits: "1234", isDefault: false));
-    paymentMethods
-        .add(PaymentMethod(id: 2, lastFourDigits: "1234", isDefault: true));
-    paymentMethods
-        .add(PaymentMethod(id: 3, lastFourDigits: "1234", isDefault: false));
-    paymentMethods
-        .add(PaymentMethod(id: 4, lastFourDigits: "1234", isDefault: false));
-
+    for (var item in response.json()) {
+      paymentMethods.add(PaymentMethod.fromJson(item));
+    }
     return paymentMethods;
   }
 
   Future<void> setDefaultPaymentMethods(int index) async {
-    // final response = await Requests.patch(
-    //   '${Config.serverUrl}/api/v1/user/me/paymentMethods/${paymentMethods[index].id}/setDefault',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // );
-    // response.raiseForStatus();
+    final response = await Requests.patch(
+      '${Config.getServerUrl()}/api/v1/user/me/paymentMethods/${paymentMethods[index].id}/setDefault',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    response.raiseForStatus();
 
-    await Future.delayed(const Duration(seconds: 1));
     for (var paymentMethod in paymentMethods) {
       paymentMethod.isDefault = false;
     }
@@ -70,21 +56,16 @@ class PaymentMethodController extends GetxController {
   }
 
   Future<void> addPaymentMethods(AddPaymentMethodRequest req) async {
-    // final response = await Requests.post(
-    //   '${Config.serverUrl}/api/v1/user/me/paymentMethods',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   json: req.toJson(),
-    // );
-    // response.raiseForStatus();
+    final response = await Requests.post(
+      '${Config.getServerUrl()}/api/v1/user/me/paymentMethods',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      json: req.toJson(),
+    );
+    response.raiseForStatus();
 
-    // paymentMethods.add(PaymentMethod.fromJson(jsonDecode(response.body)));
-    // paymentMethods.refresh();
-
-    await Future.delayed(const Duration(seconds: 1));
-    paymentMethods
-        .add(PaymentMethod(id: 1, lastFourDigits: "1234", isDefault: false));
+    paymentMethods.add(PaymentMethod.fromJson(response.json()));
     paymentMethods.refresh();
   }
 }
