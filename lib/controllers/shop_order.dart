a22e86c09926bc83f1877aac145ef3c4122de674
nonya <nonya579@gmail.com>
2022-11-13 16:40:37 +0700

    backend shop order list

--- lib/controllers/shop_order.dart
+++ lib/controllers/shop_order.dart
@@ -1,10 +1,12 @@
-import 'dart:convert';
-
 import 'package:get/get.dart';
 import 'package:giys_frontend/models/order.dart';
+import 'package:giys_frontend/models/shop.dart';
+import 'package:requests/requests.dart';
 
+import '../config/config.dart';
+
+// TODO: get my shop id
 class ShopOrderController extends GetxController {
-  // final shop = Shop().obs;
   final orders = <Order>[].obs;
 
   @override
@@ -13,110 +15,78 @@ class ShopOrderController extends GetxController {
     await updateShopOrders();
   }
 
-  Future<void> updateShopOrders() async {
-    orders.value = await getShopOrders();
+  Future<Shop> getShop(int shopId) async {
+    final response = await Requests.get(
+      '${Config.getServerUrl()}/api/v1/shops/$shopId',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    response.raiseForStatus();
+
+    return Shop.fromJson(response.json()["shop"]);
   }
 
-  Future<List<Order>> getShopOrders() async {
-    // ignore: todo
-    // TODO: get shop id
+  Future<void> updateShopOrders() async {
+    orders.value = await getShopOrders(1);
+  }
 
-    // final response = await Requests.get(
-    //   '${Config.serverUrl}/api/v1/shops/${shopId}/orders',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // );
-    // response.raiseForStatus();
-    // final orders =
-    //     jsonDecode(response.body).map((json) => Order.fromJson(json));
+  Future<List<Order>> getShopOrders(int shopId) async {
+    final response = await Requests.get(
+      '${Config.getServerUrl()}/api/v1/shops/$shopId/orders',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    response.raiseForStatus();
+    final orders = (response.json()["result"] as List)
+        .map((json) => Order.fromJson(json))
+        .toList();
 
-    // TODO: add shop to order
+    final shop = await getShop(shopId);
+    for (var order in orders) {
+      order.shop = shop;
+    }
 
-    // return orders;
-
-    await Future.delayed(const Duration(seconds: 1));
-    final orders = <Order>[];
-    orders.add(Order(
-      id: 1,
-      userId: "1",
-      shopId: 1,
-      status: "COMPLETED",
-      items: <OrderItem>[],
-    ));
-    orders.add(Order(
-      id: 2,
-      userId: "2",
-      shopId: 1,
-      status: "COMPLETED",
-      items: <OrderItem>[],
-    ));
-    orders.add(Order(
-      id: 3,
-      userId: "3",
-      shopId: 1,
-      status: "READY",
-      items: <OrderItem>[],
-    ));
-    orders.add(Order(
-      id: 4,
-      userId: "4",
-      shopId: 1,
-      status: "IN_QUEUE",
-      items: <OrderItem>[],
-    ));
-
-    // var shops = await Future.wait(orders.map(((order) => getShop(order))));
-    // shops.asMap().forEach((idx, shop) {
-    //   orders[idx].shop = shop;
-    // });
     return orders;
   }
 
-  Future<String> getShop(Order order) async {
-    await Future.delayed(const Duration(seconds: 1));
-    return "${order.userId} shop";
-  }
-
   Future<void> cancelOrder(int index) async {
-    // final response = await Requests.patch(
-    //   '${Config.serverUrl}/api/v1/user/orders/cancel',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // );
-    // response.raiseForStatus();
+    final response = await Requests.patch(
+      '${Config.getServerUrl()}/api/v1/shops/${orders[index].shopId}/orders/${orders[index].id}/cancel',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    response.raiseForStatus();
 
-    await Future.delayed(const Duration(seconds: 1));
-    orders[index].status = "CANCELED";
+    orders[index].status = response.json()["status"];
     orders.refresh();
   }
 
   Future<void> readyOrder(int index) async {
-    // final response = await Requests.patch(
-    //   '${Config.serverUrl}/api/v1/user/orders/ready',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // );
-    // response.raiseForStatus();
+    final response = await Requests.patch(
+      '${Config.getServerUrl()}/api/v1/shops/${orders[index].shopId}/orders/${orders[index].id}/ready',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    response.raiseForStatus();
 
-    await Future.delayed(const Duration(seconds: 1));
-    orders[index].status = "READY";
+    orders[index].status = response.json()["status"];
     orders.refresh();
   }
 
   Future<void> completeOrder(int index) async {
-    // final response = await Requests.patch(
-    //   '${Config.serverUrl}/api/v1/user/orders/confirm',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // );
-    // response.raiseForStatus();
+    final response = await Requests.patch(
+      '${Config.getServerUrl()}/api/v1/shops/${orders[index].shopId}/orders/${orders[index].id}/complete',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    response.raiseForStatus();
 
-    await Future.delayed(const Duration(seconds: 1));
-    orders[index].status = "COMPLETED";
+    orders[index].status = response.json()["status"];
     orders.refresh();
   }
 }
